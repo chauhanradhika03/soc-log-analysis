51cdf1a3a666f7180bd1d355a63327a9f526a9d3
chauhanradhika03 <chauhan.radhika2005@gmail.com>
2026-05-03 03:07:23 +0530

    Update log_analysis.sh

--- log_analysis.sh
+++ log_analysis.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+echo "Failed SSH login attempts"
+sudo journalctl -u ssh | grep "Failed"
+
+echo "Total failed attempts"
+sudo journalctl -u ssh | grep "Failed" | wc -l
+
+echo "Attacker IPs"
+sudo journalctl -u ssh | grep "Failed" | awk '{print $(NF-3)}'
+
+echo "Top attacking IPs"
+sudo journalctl -u ssh | grep "Failed" | awk '{print $(NF-3)}' | sort | uniq -c | sort -nr
